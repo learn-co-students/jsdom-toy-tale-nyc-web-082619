ad61c3767cacd9751ceb121843b423aa6260c286
Eugenia Kim <elk303@nyu.edu>
2019-10-10 18:55:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+const toyFormSubmit = document.querySelector(".submit")
+
 let addToy = false
 
-// YOUR CODE HERE
+function addToyCard(toy){
+  let newCard = toyCollection.insertAdjacentHTML("beforeend",
+    `<div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p><span class="likesCount"> ${toy.likes} </span></p>
+      <button data-action="like" data-id=${toy.id} class="like-btn">Like <3</button>
+    </div>`)
+}
+
+
+fetch('http://localhost:3000/toys') 
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
+    json.forEach(function(toy){
+      return addToyCard(toy)
+    })
+  })
 
 addBtn.addEventListener('click', () => {
-  // hide & seek with the form
+
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
@@ -14,5 +36,47 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+toyFormSubmit.addEventListener('click', function(event){
+  let toyName = document.querySelector("#toy-name")
+  let toyImage = document.querySelector("#toy-image")
 
-// OR HERE!
+  fetch('http://localhost:3000/toys',{
+    method: "POST",
+    headers: 
+      {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    body: 
+      JSON.stringify({
+        name: toyName.value,
+        image: toyImage.value,
+        likes: 0
+      })
+  })
+})
+
+toyCollection.addEventListener('click', function(event){
+  if (event.target.dataset.action === "like"){
+    
+    let likesCount = event.target.previousElementSibling.firstElementChild.innerText  
+    let newLikeCount = parseInt(likesCount) + 1
+    let toyId = event.target.dataset.id
+  
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: "PATCH",
+      headers: 
+        {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+      body:
+          JSON.stringify({
+          likes: newLikeCount
+        })
+    })
+  
+    let likes = event.target.previousElementSibling.firstElementChild
+    likes.innerText = newLikeCount
+  }
+})
